b8ff7b1cf665faa45fdc4084909db7669b4219a3
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2022-12-30 20:25:50 +0100

    DaisyUI & TailwindCSS, Navbar

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				"logo": "Pacifico",
+			}
+		}
+	},
+	plugins: [
+		require('daisyui')
+	]
+};
+
+module.exports = config;
