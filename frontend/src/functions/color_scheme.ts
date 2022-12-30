110402a2d5795e51bc25f9627a3913f1ce5dacbf
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2022-12-30 19:29:12 +0100

    Sveltekit setup

--- frontend/src/functions/color_scheme.ts
+++
@@ -1,8 +0,0 @@
-export function isDarkTheme() {
-    if (window.matchMedia) {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            return true;
-        }
-    }
-    return false;
-}
--- frontend/src/models/sheet.ts
+++
@@ -1,9 +0,0 @@
-export interface Sheet {
-    id: number;
-    publisher: string;
-    sheet_url: string;
-    title: string;
-    created: string;
-    author: string;
-}
-
